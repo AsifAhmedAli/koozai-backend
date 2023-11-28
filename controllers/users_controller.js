e66ca1f3339fd67fdb06dddde03f27698be296a7
AsifAhmedAli <aaali.bscs15seecs@seecs.edu.pk>
2023-11-29 01:53:07 +0500

    Update users_controller.js

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -7,7 +7,7 @@ const moment = require('moment');
 const cloudinary = require("cloudinary").v2;
 const multer = require("multer");
 const cron = require("node-cron");
-
+var request = require("request");
 // Configure Cloudinary
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
